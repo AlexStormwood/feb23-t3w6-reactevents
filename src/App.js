98c2fcd5397d026b937dccb9397b7b2d0e57cbb7
Alex H <alex.holder@bigfootds.com>
2023-09-19 20:26:23 +1000

    feat: buttons modify pokemon count

--- src/App.js
+++ src/App.js
@@ -17,7 +17,17 @@ class App extends React.Component {
 
     // start with at least ONE pokemon on screen 
     this.state = {
-      pokemonCount: 1
+      pokemonCount: 3
+    }
+  }
+
+  exampleFunction(){
+    console.log("Hello world, from button click");
+  }
+
+  decreasePokemonCount(currentCount){
+    if (currentCount > 0){
+      this.setState({pokemonCount: this.state.pokemonCount - 1});
     }
   }
 
@@ -26,7 +36,17 @@ class App extends React.Component {
       <div>
         <h1>Pokemon page</h1>
 
+        {/* <button onClick={this.exampleFunction}>Increase Pokemon count</button>
+        <button onClick={() => {console.log("Hello world from inline expression")}}>Decrease Pokemon count</button> */}
+
+        <button onClick={() => {this.setState({pokemonCount: this.state.pokemonCount + 1})}}>Increase Pokemon count</button>
+        <button onClick={() => {this.decreasePokemonCount(this.state.pokemonCount)}}>
+          Decrease Pokemon count
+        </button>
+
         <Pokemon></Pokemon>
+
+
       </div>
     )
   }
